e7e7b843dec2ba8a9c7d2f504b04166546f14d8f
legend <58605411+legend2d@users.noreply.github.com>
2023-02-23 20:55:18 +0300

    Update tablefuncs.lua

--- tablefuncs.lua
+++ tablefuncs.lua
@@ -11,4 +11,32 @@ end
 local myexample = table.hash(10, 1000)
 print(table.concat(myexample, ","))
 
+[[ -- ]]
+
+function table.findIndex(t, randomorindex)
+	if (randomorindex) then
+		for index, v in pairs(t) do
+			if tonumber(index) == tonumber(randomorindex) then
+				return t[tonumber(randomorindex)]
+			end
+		end
+	else
+		local find_random = math.random(1, #t)
+		return t[tonumber(find_random)]
+	end
+end
+
+
+-- use
+local myTable = {
+    [1] = "Hi",
+    [2] = "What's goin' on?",
+    [3] = "Nice to meet with you"
+}
+
+local finded_index = table.findIndex(myTable, 1)
+print("Finded: "..finded_index)
+
+local finded = table.findIndex(myTable, false)
+print("Generated: "..finded)
 
